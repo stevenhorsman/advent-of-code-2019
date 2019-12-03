a765c4e6d76deac187a31ec40568996f9e92e249
stevenhorsman <steven@uk.ibm.com>
2019-12-03 11:51:47 +0000

    Tidied solution more
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- day-03/crossed_wires.py
+++ day-03/crossed_wires.py
@@ -3,22 +3,18 @@ import sys
 input_file = 'day-03/input.txt'
 
 def produce_tick_map(path_string):
+    deltas = dict(zip('RLUD', [(1, 0), (-1, 0), (0, 1), (0, -1)]))
+    
     x = 0
     y = 0 
     tick = 0
     wire_path = {}
     for path in path_string:
         direction = path[:1]
-        length = path [1:]
-        for _ in range(int(length)):
-            if direction == 'R':
-                x+=1
-            if direction == 'L':
-                x-=1
-            if direction == 'U':
-                y+=1
-            if direction == 'D':
-                y-=1
+        length = int(path [1:])
+        for _ in range(length):
+            x += deltas[direction][0]
+            y += deltas[direction][1]
             point = (x,y)
             tick+=1
             if point not in wire_path:
@@ -26,14 +22,14 @@ def produce_tick_map(path_string):
     return wire_path
 
 def calculate_wire_overlaps(input, overlap_value_fn):
-    lines = [i.strip() for i in input.splitlines()]
+    lines = input.split() # str.split without any arguments splits on any whitespace and essentially strips leading/trailing whitespace.
     wire1 = lines[0].split(",")
     wire2 = lines[1].split(",")
 
     wire1_path = produce_tick_map(wire1)
     wire2_path = produce_tick_map(wire2)
 
-    crosses = set(wire1_path.keys()) & set(wire2_path.keys())
+    crosses = wire1_path.keys() & wire2_path.keys() # keys are sets in python == set(wire1_path.keys()) & set(wire2_path.keys())
     cross_distances = [overlap_value_fn(wire1_path, wire2_path, x) for x in crosses]
 
     return min(cross_distances)

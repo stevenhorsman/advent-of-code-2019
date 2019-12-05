ecae1627a75e9aac6463a773c6196ae7c1dbb494
stevenhorsman <steven@uk.ibm.com>
2019-12-05 11:08:13 +0000

    Refactor day 2 assembler
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -6,36 +6,40 @@ class IntCode:
         self.runFunction = runFunction
         self.ip_offset = ip_offset
 
-    def run(self, memory, input1, input2, output):
-       result = self.runFunction(memory, input1, input2)
-    #    print(self.name, memory[input1], memory[input2], "=", result)
-       memory[output] = result
+    def run(self, memory, offset):
+        output, result = self.runFunction(memory, offset)
+        if (output != None):
+            memory[output] = result
 
 opcodes = {}
-def addOpCode(opcode, name, op, ip_offset):  
+def addOpCode(opcode, name, op, ip_offset):
     opcodes[opcode] = IntCode(name, op, ip_offset)
 
-addOpCode(1, 'add', lambda memory, noun, verb: memory[noun] + memory[verb],4)
-addOpCode(2, 'mul', lambda memory, noun, verb: memory[noun] * memory[verb],4)
+addOpCode(1, 'add', lambda memory, arguments: (arguments[2], memory[arguments[0]] + memory[arguments[1]]), 4)
+addOpCode(2, 'mul', lambda memory, arguments: (arguments[2], memory[arguments[0]] * memory[arguments[1]]), 4)
 # addOpCode(3, 'input', lambda memory, noun: memory[noun],2)
 # addOpCode(4, 'output', lambda memory, noun: memory[noun],2)
-addOpCode(99, 'halt', lambda memory, noun, verb: None,1)
+addOpCode(98, 'seti', lambda memory, arguments: (arguments[1], arguments[0]), 3)
+addOpCode(99, 'halt', lambda memory, arguments: (None, None), 1)
 
-def execute(inputs):
-    inputs = [int(i) for i in inputs]
-    instruction_pointer = 0
-    while True:
-        instruction_pointer = execute_instruction(inputs, instruction_pointer)
-        if inputs[instruction_pointer] == 99:
-            return inputs
+def execute(memory):
+  memory = [int(i) for i in memory]
+  instruction_pointer = 0
+  while True:
+    instruction_pointer = execute_instruction(memory, instruction_pointer)
+    if memory[instruction_pointer] == 99:
+      return memory
 
-def execute_instruction(inputs, instruction_pointer = 0):
-    opcode = inputs[instruction_pointer]
-    if opcode in opcodes.keys(): 
-        curr_inst = opcodes[opcode]
-        a, b, c = inputs[instruction_pointer + 1], inputs[instruction_pointer + 2], inputs[instruction_pointer + 3]
-        curr_inst.run(inputs,a,b,c)
-        instruction_pointer += curr_inst.ip_offset
-    else: 
-        sys.stderr.write("Error - IP opcode" + inputs[instruction_pointer])
-    return instruction_pointer
+def execute_instruction(memory, instruction_pointer=0):
+  opcode = memory[instruction_pointer]
+  if opcode in opcodes.keys():
+    curr_inst = opcodes[opcode]
+    ip_offset = curr_inst.ip_offset
+    arguments = memory[instruction_pointer +
+                        1:instruction_pointer + ip_offset]
+    # TODO slice the number of arguments
+    curr_inst.run(memory, arguments)
+    instruction_pointer += ip_offset
+  else:
+    sys.stderr.write("Error - IP opcode" + memory[instruction_pointer])
+  return instruction_pointer
--- assembler/test_assembler.py
+++ assembler/test_assembler.py
@@ -1,20 +1,24 @@
 import assembler
 
-def test_part1_example_1():
+def test_add_example_1():
     check_execute("1,0,0,0,99", "2,0,0,0,99")
 
-def test_part1_example_2():
+def test_mul_example_2():
     check_execute("2,3,0,3,99", "2,3,0,6,99")
 
-def test_part1_example_3():
+def test_mul_99_99_3():
     check_execute("2,4,4,5,99,0", "2,4,4,5,99,9801")
+
+def test_seti():
+    check_execute("98,1,0,99", "1,1,0,99")
 
 def test_part1_example_4():
     check_execute("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")
 
 def test_part1_example_5():
-    check_execute("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50")
-
+    check_execute("1,9,10,3,2,3,11,0,99,30,40,50",
+                  "3500,9,10,70,2,3,11,0,99,30,40,50")
+                  
 def check_execute(input, output):
     data = input.split(",")
     assert assembler.execute(data) == [int(i) for i in output.split(",")]

c780d2ffdf6d17aabec19251c332d9c18412bd67
Steven Horsman <steven@uk.ibm.com>
2019-12-05 08:16:18 +0000

    Day 5 initial - not implemented
    Signed-off-by: Steven Horsman <steven@uk.ibm.com>

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,9 +1,10 @@
 import sys
 
 class IntCode:
-    def __init__(self, name, runFunction):
+    def __init__(self, name, runFunction, ip_offset):
         self.name = name
         self.runFunction = runFunction
+        self.ip_offset = ip_offset
 
     def run(self, memory, input1, input2, output):
        result = self.runFunction(memory, input1, input2)
@@ -11,19 +12,20 @@ class IntCode:
        memory[output] = result
 
 opcodes = {}
-def addOpCode(opcode, name, op):  
-    opcodes[opcode] = IntCode(name, op)
+def addOpCode(opcode, name, op, ip_offset):  
+    opcodes[opcode] = IntCode(name, op, ip_offset)
 
-addOpCode(1, 'add', lambda memory, noun, verb: memory[noun] + memory[verb])
-addOpCode(2, 'mul', lambda memory, noun, verb: memory[noun] * memory[verb])
-addOpCode(99, 'halt', lambda memory, noun, verb: None)
+addOpCode(1, 'add', lambda memory, noun, verb: memory[noun] + memory[verb],4)
+addOpCode(2, 'mul', lambda memory, noun, verb: memory[noun] * memory[verb],4)
+# addOpCode(3, 'input', lambda memory, noun: memory[noun],2)
+# addOpCode(4, 'output', lambda memory, noun: memory[noun],2)
+addOpCode(99, 'halt', lambda memory, noun, verb: None,1)
 
 def execute(inputs):
     inputs = [int(i) for i in inputs]
     instruction_pointer = 0
     while True:
-        execute_instruction(inputs, instruction_pointer)
-        instruction_pointer += 4
+        instruction_pointer = execute_instruction(inputs, instruction_pointer)
         if inputs[instruction_pointer] == 99:
             return inputs
 
@@ -33,6 +35,7 @@ def execute_instruction(inputs, instruction_pointer = 0):
         curr_inst = opcodes[opcode]
         a, b, c = inputs[instruction_pointer + 1], inputs[instruction_pointer + 2], inputs[instruction_pointer + 3]
         curr_inst.run(inputs,a,b,c)
+        instruction_pointer += curr_inst.ip_offset
     else: 
         sys.stderr.write("Error - IP opcode" + inputs[instruction_pointer])
-    return inputs
+    return instruction_pointer
---
+++ day-05/sunny_with_asteroids.py
@@ -0,0 +1,22 @@
+import assembler_2
+
+input_file = 'day-05/input.txt'
+
+def part1(input, noun="12", verb="2"):
+    inputs = input.split(",").copy()
+    inputs[1] = noun
+    inputs[2] = verb
+    result = assembler.execute(inputs)
+    return result[0]
+
+def part2(input):
+    for noun in range(1, 100):
+        for verb in range(1, 100):
+            if part1(input, str(noun), str(verb)) == 19690720:
+                return 100 * noun + verb
+
+if __name__ == "__main__":
+    with open(input_file) as f:
+       data = f.read()
+    print("Part 1: ", part1(data))
+    print("Part 2: ", part2(data))

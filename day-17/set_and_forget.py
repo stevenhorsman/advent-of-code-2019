4ffef22511bac805cc139437954f509760c8b2f9
stevenhorsman <steven@uk.ibm.com>
2019-12-23 12:47:35 +0000

    Day 17 - tidy up
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- day-17/set_and_forget.py
+++ day-17/set_and_forget.py
@@ -5,14 +5,13 @@ from collections import Counter
 
 input_file = 'day-17/input.txt'
 
-def get_direction(curr, rotation):
-  direction_string = '^<v>'
-  if rotation == 'L':
-    return direction_string[(direction_string.index(curr) + 1) % len(direction_string)]
-  elif rotation == 'R':
-    return direction_string[(direction_string.index(curr) - 1) % len(direction_string)]
-
-deltas = dict(zip('><^v', [(1, 0), (-1, 0), (0, -1), (0, 1)]))
+def get_adjacents(x, y):
+  return [
+    (x, y - 1),
+    (x - 1, y),
+    (x + 1, y),
+    (x, y + 1)
+  ]
 
 def part1(memory):
   memory = memory.split(",")
@@ -27,112 +26,14 @@ def part1(memory):
   is_intersection = lambda x,y: all(adjacent in scaffolds for adjacent in get_adjacents(x,y))
   return sum([x * y for (x,y) in scaffolds if is_intersection(x,y)])
 
-def get_adjacents(x, y):
-  return [
-    (x, y - 1),
-    (x - 1, y),
-    (x + 1, y),
-    (x, y + 1)
-  ]
+def get_direction(curr, rotation):
+  direction_string = '^<v>'
+  if rotation == 'L':
+    return direction_string[(direction_string.index(curr) + 1) % len(direction_string)]
+  elif rotation == 'R':
+    return direction_string[(direction_string.index(curr) - 1) % len(direction_string)]
 
-def count_matches(lst, x): 
-  return sum(lst[i:i+len(x)]==x for i in range(len(lst)))
-
-def remove_matches(lst, x): 
-  matches = [lst[i:i+len(x)]==x for i in range(len(lst))]
-  indices = [i for i, x in enumerate(matches) if x == True]
-  to_remove = []
-  for index in indices:
-    for i in range(index,index+len(x)):
-      to_remove.append(i)
-
-  return [lst[i] for i in range(0,len(lst)) if i not in to_remove]
-
-def replace_matches(lst, x, replace): 
-  matches = [lst[i:i+len(x)]==x for i in range(len(lst))]
-  indices = [i for i, x in enumerate(matches) if x == True]
-
-  to_remove = []
-  for index in indices:
-    lst[index] = replace
-    for i in range(index+1,index+len(x)):
-      to_remove.append(i)
-
-  return [lst[i] for i in range(0,len(lst)) if i not in to_remove]
-  
-# TODO - write a solver that follows the scaffolding until it hits the end and then works out how to turn. Should end up with: 
-# L,12,L,10,R,8,L,12,R,8,R,10,R,12,L,12,L,10,R,8,L,12,R,8,R,10,R,12,L,10,R,12,R,8,L,10,R,12,R,8,R,8,R,10,R,12,L,12,L,10,R,8,L,12,R,8,R,10,R,12,L,10,R,12,R,8
-def part2(memory):
-  memory = memory.split(",")
-  memory[0] = 2
-  robot = ShipComputer(memory)
-
-  robot.execute_until_blocked()
-  outputs = robot.get_all_outputs()
-  lines = "".join([ chr(c) for c in outputs]).splitlines()
-  scaffolds = [(x,y) for y in range(0,len(lines)) for x in range(0,len(lines[y])) if lines[y][x] == '#']
-  (x,y) = [(x,y) for y in range(0,len(lines)) for x in range(0,len(lines[y])) if lines[y][x] in '^<v>'][0]
-  curr_dir = lines[y][x]
-  
-  print("".join([ chr(c) for c in outputs]))
-  path = find_path(scaffolds, x, y, curr_dir)
-  print(path)
-
-  # TODO - create greedy algoritm and remove until nothing left (guess if size < 5?) then rank patterns, look for duplicates etc
-  (pattern,new_path) = remove_shortest_end(path)
-  (pattern_2,pattern_3) = remove_shortest_end(new_path)
-  patterns=[pattern, pattern_2, pattern_3]
-  #Check if all paths repeat
-  for i in range(0,len(patterns)):
-    possible_match = 1
-    while (count_matches(patterns[i],patterns[i][0:possible_match+1]) > 2):
-      possible_match += 1
-      temp_pattern = patterns[i][0:possible_match]
-      if len(remove_matches(patterns[i], temp_pattern)) == 0:
-        patterns[i] = temp_pattern
-
-  patterns.sort(key=len)
-  for i in range(0,len(patterns)-1):
-    if count_matches(patterns[-1],patterns[i]) > 0:
-      patterns[-1] = remove_matches(patterns[-1],patterns[i])
-
-  #Assume a's are correct pattern now
-  main_routine = replace_matches(path, patterns[0],'A')
-  main_routine = replace_matches(main_routine, patterns[1],'B')
-  main_routine = replace_matches(main_routine, patterns[2],'C')
-
-  instructions = [','.join(main_routine),','.join(patterns[0]).replace('R','R,').replace('L','L,'), ','.join(patterns[1]).replace('R','R,').replace('L','L,'), ','.join(patterns[2]).replace('R','R,').replace('L','L,'),'n']
-
-  # instructions = ['A,B,A,B,C,C,B,A,B,C', 'L,12,L,10,R,8,L,12','R,8,R,10,R,12','L,10,R,12,R,8','n']
-  for instruction in instructions:
-    inputs = [ ord(c) for c in instruction+'\n']
-    for code in inputs:
-      robot.put_input(code)
-  robot.execute_until_blocked()
-  outputs = robot.get_all_outputs()
-  print("".join([ chr(c) for c in outputs]))
-  return outputs[-1]
-
-def remove_shortest_end(path):
-  possible_match = 1
-  while (count_matches(path,path[0:possible_match+1]) > 2):
-    possible_match += 1
-
-  start_max=possible_match
-
-  possible_match = 1
-  while (count_matches(path,path[-(possible_match+1):]) > 2):
-    possible_match += 1
-  end_max=possible_match
-
-  if (start_max < end_max):
-    pattern = path[0:start_max]
-    new_path = remove_matches(path, pattern)
-  else:
-    pattern = path[-end_max:]
-    new_path = remove_matches(path, pattern)
-
-  return (pattern, new_path)
+deltas = dict(zip('><^v', [(1, 0), (-1, 0), (0, -1), (0, 1)]))
 
 def find_path(scaffolds, x, y, curr_dir):
   path = []
@@ -167,8 +68,89 @@ def find_path(scaffolds, x, y, curr_dir):
         path.append(curr_move+str(moves))
         end = True
 
+def count_matches(lst, x): 
+  return sum(lst[i:i+len(x)]==x for i in range(len(lst)))
+
+def remove_matches(lst, x): 
+  return replace_matches(lst, x, [])
+  
+def replace_matches(lst, x, replace): 
+  matches = [lst[i:i+len(x)]==x for i in range(len(lst))]
+  indices = [i for i, x in enumerate(matches) if x == True]
+
+  to_remove = []
+  for index in indices:
+    for j in range(len(replace)):
+      lst[index+j] = replace[j]
+    for i in range(index + len(replace),index + len(x)):
+      to_remove.append(i)
+
+  return [lst[i] for i in range(0,len(lst)) if i not in to_remove]
+  
+def get_first_pattern(path):
+  possible_match = 1
+  while (count_matches(path,path[0:possible_match+1]) > 2):
+    possible_match += 1
+
+  return path[0:possible_match]
+
+def get_instructions(path):
+  patterns = []
+  remaining_path = path[:]
+  for _ in range(1,3): # There are 3 patterns: A,B & C
+    pattern = get_first_pattern(remaining_path)
+    patterns.append(pattern)
+    remaining_path = remove_matches(remaining_path, pattern)
+  patterns.append(remaining_path) # Whatever remains is the last pattern
+
+  #Check if any of the patterns repeat
+  for i in range(len(patterns)):
+    possible_match = 1
+    while (count_matches(patterns[i],patterns[i][0:possible_match+1]) > 2):
+      possible_match += 1
+      temp_pattern = patterns[i][0:possible_match]
+      if len(remove_matches(patterns[i], temp_pattern)) == 0:
+        patterns[i] = temp_pattern
+
+  # Check if longest pattern contains any of the shortest ones as well
+  patterns.sort(key=len)
+  for i in range(0,len(patterns)-1):
+    if count_matches(patterns[-1],patterns[i]) > 0:
+      patterns[-1] = remove_matches(patterns[-1],patterns[i])
+
+  for (pattern, instruction) in zip(patterns, 'ABC'):
+    path = replace_matches(path, pattern, instruction)
+
+  instructions = [path] + patterns + ['n']
+  return [(','.join(instruction)).replace('R','R,').replace('L','L,') for instruction in instructions]
+
+def part2(memory):
+  memory = memory.split(",")
+  memory[0] = 2
+  robot = ShipComputer(memory)
+
+  robot.execute_until_blocked()
+  outputs = robot.get_all_outputs()
+  lines = "".join([ chr(c) for c in outputs]).splitlines()
+  scaffolds = [(x,y) for y in range(0,len(lines)) for x in range(0,len(lines[y])) if lines[y][x] == '#']
+  (x,y) = [(x,y) for y in range(0,len(lines)) for x in range(0,len(lines[y])) if lines[y][x] in '^<v>'][0]
+  curr_dir = lines[y][x]
+  
+  path = find_path(scaffolds, x, y, curr_dir)
+  instructions = get_instructions(path)
+
+  # instructions = ['A,B,A,B,C,C,B,A,B,C', 'L,12,L,10,R,8,L,12','R,8,R,10,R,12','L,10,R,12,R,8','n']
+  for instruction in instructions:
+    inputs = [ ord(c) for c in instruction+'\n']
+    for code in inputs:
+      robot.put_input(code)
+  robot.execute_until_blocked()
+  outputs = robot.get_all_outputs()
+  # print("".join([ chr(c) for c in outputs]))
+  return outputs[-1]
+
 if __name__ == "__main__":
   with open(input_file) as f:
     data = f.read()
-  print("Part 1: ", part1(data))
+  # print("Part 1: ", part1(data))
   print("Part 2: ", part2(data))

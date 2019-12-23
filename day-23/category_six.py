b035e0cc6787c765871488f55b21b205ac408fa6
stevenhorsman <steven@uk.ibm.com>
2019-12-23 11:07:51 +0000

    Day 23 tidy up
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- day-23/category_six.py
+++ day-23/category_six.py
@@ -2,50 +2,9 @@ from ship_computer import ShipComputer
 from collections import deque
 import sys
 
-#TODO - refactor and tidy
 input_file = 'day-23/input.txt'
 
-def part1(memory):
-  memory = memory.split(",")
-  computers = [0] * 50
-
-  queue = [deque() for _ in range(50)]
-
-  for i in range(50):
-    computers[i] = ShipComputer(memory, i)
-    computers[i].execute_until_blocked()
-
-  try:
-    while True:
-      for i in range(50):
-        computers[i].execute_until_blocked()
-        if queue[i]:
-          inputs = []
-          while queue[i]:
-            inputs.append(queue[i].popleft())
-          print('Computer',i,'receiving',inputs)
-          for input in inputs:
-            computers[i].put_input(input)
-        else:
-          computers[i].put_input(-1)
-        if computers[i].output.qsize() >=3:
-          print('Computer',i,'qsize',computers[i].output.qsize())
-          outputs = computers[i].get_all_outputs()
-          # TODO change outputs to SimpleQueue/FIFO
-          while len(outputs) >= 3:
-            address = outputs.pop(0)
-            x = outputs.pop(0)
-            y = outputs.pop(0)
-            print('Computer',i,'sent',address,x,y)
-            if address == 255:
-              return y
-            queue[address].append(x)
-            queue[address].append(y)
-  except StopIteration:
-    return -1
-
-
-def part2(memory):
+def run_nics(memory, exit_on_255 = False):
   memory = memory.split(",")
   computers = [0] * 50
 
@@ -60,45 +19,47 @@ def part2(memory):
 
   try:
     while True:
-      sent = 0
+      idle = True
       for i in range(50):
         computers[i].execute_until_blocked()
+
         if queue[i]:
-          sent +=1
-          inputs = []
+          idle = False
           while queue[i]:
-            inputs.append(queue[i].popleft())
-          print('Computer',i,'receiving',inputs)
-          for input in inputs:
-            computers[i].put_input(input)
+            computers[i].put_input(queue[i].popleft())
         else:
           computers[i].put_input(-1)
+
         if computers[i].output.qsize() >=3:
-          print('Computer',i,'qsize',computers[i].output.qsize())
+          idle = False
           outputs = computers[i].get_all_outputs()
-          # TODO change outputs to SimpleQueue/FIFO
           while len(outputs) >= 3:
-            address = outputs.pop(0)
-            x = outputs.pop(0)
-            y = outputs.pop(0)
-            print('Computer',i,'sent',address,x,y)
+            address, x, y = outputs.pop(0), outputs.pop(0), outputs.pop(0)
             if address == 255:
-              nat = [x,y]
+              if exit_on_255:
+                return y
+              else:
+                nat = [x,y]
             else:
-              queue[address].append(x)
-              queue[address].append(y)
-      if sent == 0 and nat[0] != None:
+              queue[address].extend([x,y])
+
+      if idle and nat[0] != None:
         if nat[1] == prev_nat_y:
           return nat[1]
         else:
-          queue[0].append(nat[0])
-          queue[0].append(nat[1])
+          queue[0].extend(nat)
           prev_nat_y = nat[1]
   except StopIteration:
     return -1
 
+def part1(memory):
+  return run_nics(memory, True)
+
+def part2(memory):
+  return run_nics(memory)
+
 if __name__ == "__main__":
   with open(input_file) as f:
     data = f.read()
-  print("Part 1: ", part1(data))
-  # print("Part 2: ", part2(data))
+  # print("Part 1: ", part1(data))
+  print("Part 2: ", part2(data))
